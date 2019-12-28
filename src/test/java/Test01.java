8d88d949add144d8de19e170e887d25977e009f8
Daerduominer <1392810090@qq.com>
2019-12-28 13:31:02 +0800

    refactor codes

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -2,9 +2,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Test01 {
+	Question01 question=new Question01();
+	
 	@Test
 	public void print_test() {
-		Question01 question=new Question01();
+		
 		Assert.assertEquals("7", question.printNumber(7));
     	Assert.assertEquals("Fizz", question.printNumber(3));
         Assert.assertEquals("Bizz", question.printNumber(5));
